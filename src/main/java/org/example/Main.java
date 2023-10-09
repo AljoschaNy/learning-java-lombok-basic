9472ef303127bee3958b292ccf88ed3bb3bd3fc6
Aljoscha Nyang <a.nyang@gmx.de>
2023-10-09 15:35:53 +0200

    Add instances of Student, Teacher and Course

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,38 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Student student1 = Student.builder()
+                .id("s1")
+                .name("Josch")
+                .address("NRW")
+                .grade("1++")
+                .build();
+        Student student2 = Student.builder()
+                .id("s2")
+                .name("Tim")
+                .address("NRW")
+                .build();
+
+        Teacher teacher1 = new Teacher("t1","Thomas","Math");
+        Teacher teacher2 = new Teacher("t2","Marie","Music");
+
+        Course course1 = Course.builder()
+                .id("c1")
+                .name("Music Beginner")
+                .teacher(teacher2)
+                .students(List.of(student1,student2))
+                .build();
+
+        Course course2 = Course.builder()
+                .id("c2")
+                .name("Math Beginner")
+                .teacher(teacher1)
+                .students(List.of(student1,student2))
+                .build();
+
     }
 }
