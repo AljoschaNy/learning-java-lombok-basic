b0828599af1e8663c3183f9b3e0d7bf8c2d28ce5
Aljoscha Nyang <a.nyang@gmx.de>
2023-10-09 16:23:21 +0200

    Create UniversityService class

---
+++ src/main/java/org/example/UniversityService.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Data
+public class UniversityService {
+    University university;
+
+
+}
