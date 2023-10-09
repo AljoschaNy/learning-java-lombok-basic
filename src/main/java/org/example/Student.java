d6107f1ffe80f63523aa40091f893e362b370189
Aljoscha Nyang <a.nyang@gmx.de>
2023-10-09 15:30:05 +0200

    Add builder annotation to Student and Course classes

--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -1,12 +1,14 @@
 package org.example;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Builder
 public class Student {
 
     private String id;
