117e30e7d0e80affd844d5e10cfa4387142462d7
Aljoscha Nyang <a.nyang@gmx.de>
2023-10-09 15:10:54 +0200

    Add lombok annotations

--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -1,5 +1,12 @@
 package org.example;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Student {
 
     private String id;
